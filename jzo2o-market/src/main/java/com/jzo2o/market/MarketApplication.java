fe70bbfce6a1ed84b0b3920c7fade324118f3423
Frank <kissingliang@qq.com>
2025-06-14 19:59:36 +0800

    线程池进行结果同步

--- jzo2o-market/src/main/java/com/jzo2o/market/MarketApplication.java
+++ jzo2o-market/src/main/java/com/jzo2o/market/MarketApplication.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.jzo2o.api")
 @Slf4j
 @MapperScan("com.jzo2o.market.mapper")
 public class MarketApplication {
--- jzo2o-market/src/main/java/com/jzo2o/market/handler/XxlJobHandler.java
+++ jzo2o-market/src/main/java/com/jzo2o/market/handler/XxlJobHandler.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 import static com.jzo2o.market.constants.RedisConstants.Formatter.*;
 import static com.jzo2o.market.constants.RedisConstants.RedisKey.COUPON_SEIZE_SYNC_QUEUE_NAME;
 
@@ -65,6 +67,30 @@ public class XxlJobHandler {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Resource(name="syncThreadPool")
+    private ThreadPoolExecutor threadPoolExecutor;
+
+    /**
+     * 抢券同步队列
+     */
+    @XxlJob("seizeCouponSyncJob")
+    public void seizeCouponSyncJob() {
+        /**
+         * 开始同步，可以使用自定义线程池，如果不设置使用默认线程池
+         *
+         * @param queueName 同步队列名称
+         * @param storageType 数据存储类型，1：redis hash数据结构，2：redis list数据结构，3：redis zSet结构
+         * @param mode 1 单条执行 2批量执行
+         * @param dataSyncExecutor 数据同步线程池
+         */
+        syncManager.start(
+                COUPON_SEIZE_SYNC_QUEUE_NAME,//抢券同步队列
+                RedisSyncQueueConstants.STORAGE_TYPE_HASH,//redis同步队列存储结构hash
+                RedisSyncQueueConstants.MODE_SINGLE,//单条执行模式
+                threadPoolExecutor//线程池
+        );
     }
 
 
